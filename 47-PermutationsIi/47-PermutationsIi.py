6197eebda0aaacd5f06022830a4aced9a575c7bf
Ajjuhspryzn <ajjuhspryzn@gmail.com>
2026-07-14 15:20:29 +0530

    Create 47-PermutationsIi

---
+++ 47-PermutationsIi/47-PermutationsIi.py
@@ -0,0 +1,16 @@
+# Last updated: 7/14/2026, 3:20:27 PM
+class Solution:
+    def permuteUnique(self, nums):
+        nums.sort()
+        result = []
+        temp_list = []
+        self.helper(nums, temp_list, result)
+        return result
+    
+    def helper(self, nums, temp_list, result):
+        if len(nums) == 0:
+            result.append(temp_list)
+            
+        for i in range(len(nums)):
+            if (i+1 < len(nums) and nums[i] != nums[i+1]) or i+1 == len(nums): # to prevent nums[i+1] out of range
+                self.helper(nums[:i]+nums[i+1:], temp_list+[nums[i]], result)
