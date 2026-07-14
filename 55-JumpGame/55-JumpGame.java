d2e2c4e5abc02968400bfb334526f0c7b02b00ae
Ajjuhspryzn <ajjuhspryzn@gmail.com>
2026-07-14 15:20:20 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,11 @@
+// Last updated: 7/14/2026, 3:20:19 PM
+class Solution {
+    public boolean canJump(int[] nums) {
+       int reachable = 0;
+       for(int i = 0; i < nums.length; i ++) {
+           if(i > reachable) return false;
+           reachable = Math.max(reachable, i + nums[i]);
+       } 
+       return true;
+    }
+}
