9402aa1a3a8246e5adaf4db8bc547c993e26cf4f
Ajjuhspryzn <ajjuhspryzn@gmail.com>
2026-08-04 12:46:46 +0530

    Create 833-FindAndReplaceinString

---
+++ 833-FindAndReplaceinString/version/python/833-FindAndReplaceinString_20260804_124645.py
@@ -0,0 +1,6 @@
+# Last updated: 8/4/2026, 12:46:45 PM
+1class Solution:
+2    def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
+3        for i ,src,tg in sorted(list(zip(indices,sources,targets)),reverse=True):
+4            if s[i:i+len(src)]==src:s=s[:i]+tg+s[i+len(src):]
+5        return s
