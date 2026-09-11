71a9bbf35b57d393c2de1d8db03cbff360d04277
AjayKumar D <ajjuhspryzn@gmail.com>
2026-09-11 12:11:59 +0530

    Create 1004-MaxConsecutiveOnesIII

---
+++ 1004-MaxConsecutiveOnesIII/version/python/1004-MaxConsecutiveOnesIII_20260911_121157.py
@@ -0,0 +1,15 @@
+# Last updated: 9/11/2026, 12:11:57 PM
+1class Solution:
+2    def longestOnes(self, nums: List[int], k: int) -> int:
+3        left=0
+4        zero=0
+5        ans=0
+6        for right in range(len(nums)):
+7            if nums[right]==0:
+8                zero+=1
+9            while zero>k:
+10                if nums[left]==0:
+11                    zero-=1
+12                left+=1
+13            ans=max(ans,right-left+1)
+14        return ans
